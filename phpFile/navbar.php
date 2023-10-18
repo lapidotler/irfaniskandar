91c3d856680d0320be59d78242e5e1b228e2bf0b
lapidotler <22024044@myrp.edu.sg>
2023-10-18 17:27:30 +0800

    Homepage (L1)

--- phpFile/navbar.php
+++ phpFile/navbar.php
@@ -1,26 +1,38 @@
-<nav class="navbar navbar-expand-sm bg-secondary navbar-light">
+<nav class="navbar navbar-expand-sm navbar-light nav-bg" style="width: 100%;">
     <div class="container-fluid">
         <a class="navbar-brand" href="homepage.php">
-            <img src="images/logo_main.png" width=auto height="70px" style="padding-left: 10px">
+            <img src="images/logo_main.png" width="auto" height="70px" style="padding-left: 10px">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="collapsibleNavbar" style="font-size: 25px; font-family: 'Futura Hv BT'">
+        <div class="collapse navbar-collapse" id="collapsibleNavbar">
             <ul class="navbar-nav nav-pills ms-auto">
-                <li class="nav-item" style="padding-right:75px;">
-                    <a class="nav-link" href="movieListPage.php">Home</a>
+                <li class="nav-item navbar-items" style="padding-right: 50px;">
+                    <a class="nav-link text-dark" href="movieListPage.php">Home</a>
                 </li>
-                <li class="nav-item" style="padding-right:75px;">
-                    <a class="nav-link" href="reviews.php">About Me</a>
+                <li class="nav-item navbar-items" style="padding-right: 50px;">
+                    <a class="nav-link text-dark" href="reviews.php">About Me</a>
                 </li>  
-                <li class="nav-item" style="padding-right:75px;">
-                    <a class="nav-link" href="contactUs.php">Portfolio</a>
+                <li class="nav-item navbar-items" style="padding-right: 50px;">
+                    <a class="nav-link text-dark" href="contactUs.php">Portfolio</a>
                 </li>  
-                <li class="nav-item" style="padding-right:25px;">
-                    <a class="nav-link" href="faq.php" >Contact Me</a>
+                <li class="nav-item navbar-items" style="padding-right: 50px;">
+                    <a class="nav-link text-dark" href="faq.php">Contact Me</a>
                 </li>
             </ul>
         </div>
     </div>
 </nav>
+
+<style>
+    .nav-bg {
+        background-color: #bbb3d6; /* Replace with your desired background color */
+    }
+    
+    .navbar-items {
+        font-size: 22px;
+        font-weight: bold;
+        font-family: "Futura Hv BT"
+    }
+</style>
