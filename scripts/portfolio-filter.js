0537c41ba19fdd4879e88491c63236dcf700f452
lapidotler <22024044@myrp.edu.sg>
2023-12-22 17:14:41 +0800

    Portfolio Update (22/12)
    - Introduced a filtering function for Portfolio Cards
    - Removed carousel + lightbox JS and CSS (considered not needed)
    - Moved styles.css & spacer.css inside "Format" folder
    - Changed each html accordingly to changes of CSS files.
    - Started working on prototype page "maldives-special.html"

---
+++ scripts/portfolio-filter.js
@@ -0,0 +1,35 @@
+// Filter Categories
+$(document).ready(function () {
+    var filtered = false;
+    $(".portfolio-option").click(function() {
+        if ($(this).hasClass("selected")) {
+            $(".portfolio-option").removeClass("selected");
+            filtered = false;
+        } else {
+            $(".portfolio-option").removeClass("selected");
+            $(this).addClass("selected");
+            filtered = true;
+        }
+
+        if (filtered) {
+            var filter = $(this).find('a').html();
+            if (filter === "Design") {
+                $(".design-works").show(500);
+                $(".development-works").hide(500);
+                $(".other-works").hide(500);
+            } else if (filter === "Development") {
+                $(".development-works").show(500);
+                $(".design-works").hide(500);
+                $(".other-works").hide(500);
+            } else if (filter === "Others") {
+                $(".other-works").show(500);
+                $(".design-works").hide(500);
+                $(".development-works").hide(500);
+            } else {
+                $(".filter-content").html("Error!"); 
+            }
+        } else {
+            $(".portfolio-category").show(500);
+        }
+    });
+});
