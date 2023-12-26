e33901dd20183e002f28f96ea5bfc19c797c210f
lapidotler <22024044@myrp.edu.sg>
2023-12-26 22:09:31 +0800

    Portfolio Update (26/12)
    - Updated Contact Page
    - Removed contact.php
    - Added GIF and edited contact form in contact.html

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,16 +1,44 @@
-// contact-form.js
+function validateForm() {
+    // Your form validation logic here
+    // Return true if the form is valid, false otherwise
 
-function submitForm() {
-    function validateForm() {
-        var name = document.getElementById("name").value;
-        var email = document.getElementById("email").value;
-        var message = document.getElementById("message").value;
+    // For now, let's assume the form is always valid
+    showThankYouMessage();
 
-        if (name === "" || email === "" || message === "") {
-            alert("Please fill in all required fields.");
-            return false;
-        }
+    // Prevent the form from submitting (if needed)
+    return false;
+}
 
-        return true;
+function showThankYouMessage() {
+    // Hide the form
+    document.getElementById('contactForm').style.display = 'none';
+
+    // Show the Thank You message
+    document.getElementById('thankYouMessage').style.display = 'block';
+
+    // Hide the email link (optional)
+    const emailLink = document.getElementById('emailLink');
+    if (emailLink) {
+        emailLink.style.display = 'none';
+    }
+
+    // Change the CSS padding of #contact-form
+    document.getElementById('contact-form').style.padding = '25px';
+
+    // Check if the email link button is already added
+    const emailLinkButton = document.getElementById('emailLinkButton');
+
+    if (!emailLinkButton) {
+        // If not, create the button and append it to the contact container
+        const button = document.createElement('button');
+        button.id = 'emailLinkButton';
+        button.className = 'contact-form-button';
+        button.innerText = 'Send Email';
+        button.onclick = function() {
+            window.location.href = 'mailto:southsoide1@gmail.com';
+        };
+
+        const contactContainer = document.querySelector('.contact-container');
+        contactContainer.appendChild(button);
     }
 }
